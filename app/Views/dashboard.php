5d32535ca825be7507b2161b548a08733f979b19
atomy <atom3123@gmail.com>
2022-05-02 09:44:50 +0700

    change

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -2,4 +2,4 @@
 <?php $session = session(); ?>
 <h3>Welcome back <?= $session->get('firstName'); ?></h3>
 <br>
-<a href="<?= base_url('/logout')?>" class="btn btn-danger">Logout</a>
+<a href="<?= base_url('/logout')?>" class="btn btn-danger">Logoutxxxxx</a>
